c9fa38955ced967c14f0393f3c7df8c301e2fe4a
paulo soyepia <90360902+soyepia@users.noreply.github.com>
2023-11-03 02:08:41 +0000

    testes incluidos com sucesso

--- index.php
+++ index.php
@@ -1,40 +1,50 @@
 <?php
-$html = file_get_contents('https://unasp.br/cursos/graduacao');
+use PHPUnit\Framework\TestCase;
 
-$dom = new DOMDocument();
-@$dom->loadHTML($html);
+class WebScrapingTest extends TestCase {
 
-$unaCardGridStageElements = $dom->getElementsByTagName('div');
-$unaCardWrapperElements = [];
+    public function testScrapingAndCSVGeneration() {
+        $html = file_get_contents('https://unasp.br/cursos/graduacao');
+        $dom = new DOMDocument();
+        @$dom->loadHTML($html);
 
-foreach ($unaCardGridStageElements as $element) {
-    $class = $element->getAttribute('class');
-    if (strpos($class, 'una-card-grid__stage') !== false) {
-        $unaCardWrapperElementsInStage = $element->getElementsByTagName('div');
-        foreach ($unaCardWrapperElementsInStage as $wrapperElement) {
-            $wrapperClass = $wrapperElement->getAttribute('class');
-            if (strpos($wrapperClass, 'una-card-wrapper') !== false) {
-                $unaCardWrapperElements[] = $wrapperElement;
+        $unaCardGridStageElements = $dom->getElementsByTagName('div');
+        $unaCardWrapperElements = [];
+
+        foreach ($unaCardGridStageElements as $element) {
+            $class = $element->getAttribute('class');
+            if (strpos($class, 'una-card-grid__stage') !== false) {
+                $unaCardWrapperElementsInStage = $element->getElementsByTagName('div');
+                foreach ($unaCardWrapperElementsInStage as $wrapperElement) {
+                    $wrapperClass = $wrapperElement->getAttribute('class');
+                    if (strpos($wrapperClass, 'una-card-wrapper') !== false) {
+                        $unaCardWrapperElements[] = $wrapperElement;
+                    }else {
+                        echo "erro ao adicionar curos no conjunto" . $wrapperElement;
+                    }
+                }
+            }else {
+                echo "erro ao capturar a class do HTML";
             }
         }
+
+        $texto = [];
+        foreach ($unaCardWrapperElements as $element) {
+            $text = $element->textContent;
+            $text = iconv('UTF-8', 'UTF-8//IGNORE', $text);
+            $texto[] = $text . "\n";
+        }
+
+        $csvFileName = 'dados.csv';
+        $csvFile = fopen($csvFileName, 'w');
+        fwrite($csvFile, "\xEF\xBB\xBF");
+        fputcsv($csvFile, array('LISTA DE CURSOS'));
+
+        foreach ($texto as $row) {
+            fputcsv($csvFile, array($row));
+        }
+        fclose($csvFile);
+
+        $this->assertFileExists($csvFileName);
     }
 }
-
-$texto = [];
-foreach ($unaCardWrapperElements as $element) {
-    $text = $element->textContent;
-    $text = iconv('UTF-8', 'UTF-8//IGNORE', $text);
-    $texto[] = $text . "\n";
-}
-
-$csvFileName = 'dados.csv';
-$csvFile = fopen($csvFileName, 'w');
-fwrite($csvFile, "\xEF\xBB\xBF");
-fputcsv($csvFile, array('LISTA DE CURSOS'));
-
-foreach ($texto as $row) {
-    fputcsv($csvFile, array($row));
-}
-fclose($csvFile);
-
-echo 'Arquivo CSV gerado com sucesso (UTF-8).';
